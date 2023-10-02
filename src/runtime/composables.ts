b8c5f310f5edea37b80993c28886cbde205db097
kevin olson <acidjazz@gmail.com>
2023-10-02 12:12:07 -0500

    💄 more types attmepts

--- src/runtime/composables.ts
+++ src/runtime/composables.ts
@@ -1,8 +1,9 @@
 import { useNuxtApp } from '#app'
+import dayjs from 'dayjs'
 
 // export declare const useDayjs: () => (string) => Dayjs;
 // $dayjs(date?: dayjs.ConfigType): dayjs.Dayjs
-export const useDayjs = () => {
+export const useDayjs = (): (date?: dayjs.ConfigType) => dayjs.Dayjs => {
   const { $dayjs } = useNuxtApp()
-  return $dayjs
+  return $dayjs as unknown as (date?: dayjs.ConfigType) => dayjs.Dayjs
 }
